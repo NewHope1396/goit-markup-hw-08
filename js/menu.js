b5d46752f2cc89fb56dd31965278d9658bcf447a
NewHope <rimondtt@gmail.com>
2021-01-26 18:46:28 +0200

    menu-mobile-done

--- js/menu.js
+++ js/menu.js
@@ -1,9 +1,14 @@
 (() => {
-    const menuBtnRef = document.querySelector("[data-menu-button]");
-
-    menuBtnRef.addEventListener("click", () => {
-
-        menuBtnRef.classList.toggle("is-open");
-
-    });
-})();
+    const refs = {
+      openMenuBtn: document.querySelector('[data-menu-open]'),
+      closeMenuBtn: document.querySelector('[data-menu-close]'),
+      menu: document.querySelector('[data-menu]'),
+    };
+  
+    refs.openMenuBtn.addEventListener('click', toggleMenu);
+    refs.closeMenuBtn.addEventListener('click', toggleMenu);
+  
+    function toggleMenu() {
+      refs.menu.classList.toggle('is-hidden');
+    }
+  })();
